33e3a6dc1198cbc33364f90e31b9c99970ad841a
Filippo Pellizzari <filippo.pellizzari109@gmail.com>
2023-11-19 13:29:10 +0100

    roc auc and feature importance

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import logging
 
 import pandas as pd
 import xgboost as xgb
-from sklearn.metrics import precision_recall_curve
+from matplotlib import pyplot as plt
+from sklearn.metrics import precision_recall_curve, roc_auc_score
 from sklearn.model_selection import train_test_split
 
 from dvclive import Live
@@ -34,23 +35,49 @@ def main() -> None:
         y_score = model.predict_proba(X_test)[:, 1]
         y_pred = model.predict(X_test)
         accuracy_test = float(model.score(X_test, y_test))
+        roc_auc_test = float(roc_auc_score(y_test, y_score))
         live.log_metric("accuracy:test", accuracy_test, plot=False)
+        live.log_metric("roc_auc:test", roc_auc_test, plot=False)
         live.log_sklearn_plot("calibration", y_test, y_score)
 
         prec, recall, _ = precision_recall_curve(
             y_test, y_score, pos_label=model.classes_[1]
         )
-        datapoints = pd.DataFrame({"precision": prec, "recall": recall}).to_dict(
+        prec_recall_dict = pd.DataFrame({"precision": prec, "recall": recall}).to_dict(
             "records"
         )
         live.log_plot(
             "precision_recall",
-            datapoints,
+            prec_recall_dict,
             x="recall",
             y="precision",
             template="linear",
             title="Precision Recall plot",
         )
+
+        feat_importance_dict = (
+            pd.DataFrame(
+                {
+                    "feature": model.feature_names_in_,
+                    "importance": model.feature_importances_,
+                }
+            )
+            .sort_values("importance", ascending=False)
+            .to_dict("records")
+        )
+        live.log_plot(
+            "feature_importance",
+            feat_importance_dict,
+            x="importance",
+            y="feature",
+            template="bar_horizontal",
+            title="Feature importance",
+        )
+        fig, _ = plt.subplots()
+        xgb.plot_importance(model)
+        plt.tight_layout()
+        live.log_image("feature_importance.png", fig)
+
         live.log_sklearn_plot("confusion_matrix", y_test, y_pred, name="cm.json")
         live.make_report()
     logging.info("End")
