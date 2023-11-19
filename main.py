094db6e7b6142be328fb8ad708ce7ca745e0bfba
Filippo Pellizzari <filippo.pellizzari109@gmail.com>
2023-11-19 12:15:45 +0100

    precision recall plot

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import pandas as pd
 import xgboost as xgb
 from dvclive import Live
+from sklearn.metrics import precision_recall_curve
 from sklearn.model_selection import train_test_split
 
 
@@ -17,26 +18,41 @@ def main() -> None:
     train, test = train_test_split(df, random_state=0, stratify=df["labels"])
     train, val = train_test_split(train, random_state=0, stratify=train["labels"])
     X_train = train.drop(columns=["labels"])
-    y_train = train["labels"]
+    y_train = train["labels"].values
     X_val = val.drop(columns=["labels"])
-    y_val = val["labels"]
+    y_val = val["labels"].values
     X_test = test.drop(columns=["labels"])
-    y_test = test["labels"]
+    y_test = test["labels"].values
     logging.info("Model training")
     with Live("custom_dir") as live:
         model = xgb.XGBClassifier(
             random_state=0,
-            n_estimators=50,
+            n_estimators=200,
         )
 
         model.fit(X_train, y_train)
+        logging.info("Evaluation")
         y_score = model.predict_proba(X_test)[:, 1]
         y_pred = model.predict(X_test)
-        accuracy = float(model.score(X_test, y_test))
-        live.log_metric("accuracy", accuracy, plot=True)
-        live.log_sklearn_plot("calibration", y_test.values, y_score)
-        # live.log_sklearn_plot("roc", y_test.values, y_score)
-        live.log_sklearn_plot("confusion_matrix", y_test.values, y_pred, name="cm.json")
+        accuracy_test = float(model.score(X_test, y_test))
+        live.log_metric("accuracy:test", accuracy_test)
+        live.log_sklearn_plot("calibration", y_test, y_score)
+
+        prec, recall, _ = precision_recall_curve(
+            y_test, y_score, pos_label=model.classes_[1]
+        )
+        datapoints = pd.DataFrame({"precision": prec, "recall": recall}).to_dict(
+            "records"
+        )
+        live.log_plot(
+            "precision_recall",
+            datapoints,
+            x="recall",
+            y="precision",
+            template="linear",
+            title="Precision Recall plot",
+        )
+        live.log_sklearn_plot("confusion_matrix", y_test, y_pred, name="cm.json")
     logging.info("End")
 
 
