ac47d89961ac95b58f35e9ca14cd3ae8a31b4bf9
Filippo Pellizzari <filippo.pellizzari109@gmail.com>
2024-02-12 12:12:06 +0100

    mlflow tracking

--- 02_evaluation.py
+++ 02_evaluation.py
@@ -1,8 +1,10 @@
 import logging
 
+import mlflow
 import pandas as pd
 import xgboost as xgb
 from matplotlib import pyplot as plt
+from mlflow.data.pandas_dataset import PandasDataset
 from sklearn.metrics import precision_recall_curve, roc_auc_score
 
 from dvclive import Live
@@ -23,6 +25,7 @@ def main() -> None:
     y_pred = model.predict(X_test)
     accuracy_test = float(model.score(X_test, y_test))
     roc_auc_test = float(roc_auc_score(y_test, y_score))
+    logging.info("DVC live")
     with Live() as live:
         live.log_metric("accuracy:test", accuracy_test, plot=False)
         live.log_metric("roc_auc:test", roc_auc_test, plot=False)
@@ -47,6 +50,21 @@ def main() -> None:
         live.log_image("feature_importance.png", fig)
         # confusion matrix
         live.log_sklearn_plot("confusion_matrix", y_test, y_pred, name="cm.json")
+
+    logging.info("MLFlow")
+    mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
+    mlflow.set_experiment("MLflow Quickstart")
+    test_set: PandasDataset = mlflow.data.from_pandas(X_test)
+    with mlflow.start_run():
+        mlflow.log_metric("accuracy-test", accuracy_test)
+        mlflow.log_metric("roc_auc-test", roc_auc_test)
+
+        signature = mlflow.models.infer_signature(X_test, model.predict(X_test))
+
+        model_info = mlflow.xgboost.log_model(model, "classifier", signature=signature)
+        mlflow.log_input(test_set, context="testing")
+        mlflow.log_figure(fig, "feature_importance.png")
+
     logging.info("End")
 
 
